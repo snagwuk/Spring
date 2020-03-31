f1a32dcfd08a68ee3613684351ced65fe45adc0b
snagwuk <snagwuk@users.noreply.github.com>
2020-03-31 09:12:11 +0900

    Spring_chap09

---
+++ Spring_chap09/src/main/java/chap09/gm/GuestMessageList2.java
@@ -0,0 +1,20 @@
+package chap09.gm;
+
+import java.util.List;
+
+public class GuestMessageList2
+{
+    
+    private List<GuestMessage> messages;
+    
+    public GuestMessageList2(List<GuestMessage> messages)
+    {
+        this.messages = messages;
+    }
+    
+    public List<GuestMessage> getMessages()
+    {
+        return messages;
+    }
+    
+}
