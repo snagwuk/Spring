eb376ba07e4c2ea86fb974bb98d8fc321b0caf77
snagwuk <snagwuk@users.noreply.github.com>
2020-04-24 10:47:36 +0900

    Spring_chap10

---
+++ Spring_chap10/src/main/java/chap10/springconf/RootConfig.java
@@ -0,0 +1,8 @@
+package chap10.springconf;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RootConfig {
+
+}
---
+++ Spring_chap10/src/main/java/chap10/springconf/WebConfig.java
@@ -0,0 +1,30 @@
+package chap10.springconf;
+
+import chap10.web.HelloController;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig
+{
+    
+    @Bean
+    public ViewResolver viewResolver()
+    {
+        InternalResourceViewResolver vr = new InternalResourceViewResolver();
+        vr.setPrefix("/WEB-INF/view/");
+        vr.setSuffix(".jsp");
+        return vr;
+    }
+    
+    @Bean
+    public HelloController helloController()
+    {
+        return new HelloController();
+    }
+}
